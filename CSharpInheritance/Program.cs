1b193a8e4fc50dbebd7c5958357b08b91363b11d
Asmaa Mostafa <asmaalnager1872004@gmail.com>
2026-02-05 22:04:48 +0200

    taskinher

---
+++ CSharpInheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using ConsoleApp1;
+using System;
+namespace consoleAPP1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new SalesMan(1, "asmaa", 200, 100, 2);
+            Console.WriteLine(employee);
+            employee.Print();
+            Console.WriteLine("\n////////////////");
+            Employee employee1 = new ACounter(2,"ahmed",200, 100, 2);
+          employee1.Print();
+            SalesMan salesmain =new SalesMan(3,"mohmed",300,100, 2);
+            Console.WriteLine("\n////////////////");
+            Console.WriteLine(salesmain);
+            salesmain.Print();
+            Console.WriteLine("\n////////////////");
+            ACounter aCounter = new ACounter(4,"mostafa",100,3,1);
+            aCounter.Print();
+            Console.WriteLine("\n////////////////");
+
+
+
+
+        }
+    }
+}
+
+
+
---
+++ CSharpInheritance/employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+        public virtual double Getsalary() { return salary; }
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Salary: {Getsalary()}");
+
+
+
+        }
+
+
+    }
+    class ACounter : Employee
+    {
+        public double prjectacount;
+        public double horecount;
+ public ACounter (int id, string name, double salary, double prjectacount, double horecount):base(id,name,salary)
+        {
+            this.prjectacount= prjectacount;
+            this.horecount= horecount;
+        }
+        public override double Getsalary()
+        {
+            double resul = salary + (prjectacount * horecount);
+            return resul;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"projectacount: {prjectacount},horecount: {horecount}");
+
+
+        }
+    }
+
+    class SalesMan : Employee
+    {
+        public double comission;
+        public int projectcount;
+
+        public SalesMan(int id, string name, double salary ,double comission, int projectcount):base(id,name,salary) 
+        {
+            this.comission = comission;
+            this.projectcount = projectcount;
+        }
+
+        public override double Getsalary()
+        {
+            comission = projectcount * 200;
+            return salary + comission;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"projects: {projectcount}");
+            Console.WriteLine($" comission: {projectcount * 200}");
+
+        }
+
+
+    }
+}
+
