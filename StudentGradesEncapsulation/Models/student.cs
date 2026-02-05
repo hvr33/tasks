18eb68558a5feb6a858c058e805a5be66cedf877
Asmaa Mostafa <asmaalnager1872004@gmail.com>
2026-02-05 22:05:56 +0200

    tasken

---
+++ StudentGradesEncapsulation/Models/student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppsreuct2.Models
+{
+ public  struct Student
+    {
+       
+             
+           private int id;
+           private string name;
+        private double grad;
+       private bool passed;
+
+        public double Grad {
+            get { return grad; }
+            set
+            {
+                if (value>=0&&value<=100)
+                {
+                    grad = value;
+                }
+                else {
+                    Console.WriteLine("Invalid grade. Please enter a value between 0 and 100.");
+                }
+            }
+        
+        
+        }
+
+        public bool Passed
+        {
+           
+
+            get { return Grad >= 50;
+
+            }
+
+        }
+      public Student(int id,string name,double grad)
+        {
+            this.id = id;
+            this.name = name;
+            this.grad = 0;
+            Grad = grad;
+
+        }
+
+       
+      
+        public int GetGradeAsInt()
+        {
+          return (int)grad  ;
+        }
+        public override string ToString()
+        {
+            return $"ID: {id}, Name: {name}, Grade: {GetGradeAsInt()}, Passed: {Passed}";
+        }
+    }
+}
---
+++ StudentGradesEncapsulation/Program.cs
@@ -0,0 +1,87 @@
+﻿using ConsoleAppsreuct2.Models;
+using System;   
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace ConsoleAppsreuct2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of students: ");
+            int size = int.Parse(Console.ReadLine());
+            Student[] students = new Student[size];
+           Student student=new Student();
+          
+            for (int i = 0; i < size; i++)
+            {
+                 Console.WriteLine($"\nStudent: {i + 1}");
+                Console.WriteLine("Enter Student ID");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Student Name");
+                string name = Console.ReadLine();
+                Console.WriteLine("ENte Stutend grad");
+                int grad = int.Parse(Console.ReadLine());
+                students[i]=new Student(id,name,grad);
+               
+         
+               
+               
+
+            };
+            display(students);
+        }
+      static public void display(Student[] students)
+        {
+            double totalGrades = 0;
+            int passedCount = 0;
+            int failedCount = 0;
+
+            Console.WriteLine("\n--- Students Data ---");
+            foreach (Student s in students)
+            {
+                Console.WriteLine(s); // ToString()
+                totalGrades += s.Grad;
+
+                if (s.Passed)
+                    passedCount++;
+                else
+                    failedCount++;
+            }
+
+            double average = totalGrades / students.Length;
+
+            Console.WriteLine("\n--- Statistics ---");
+            Console.WriteLine($"Average Grade: {average}");
+            Console.WriteLine($"Passed Students: {passedCount}");
+            Console.WriteLine($"Failed Students: {failedCount}");
+        }
+    }
+
+
+
+}
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
