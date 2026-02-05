a582fafff26c45c88c0b14e678a5fb3dd7720d17
Asmaa Mostafa <asmaalnager1872004@gmail.com>
2026-02-05 22:55:15 +0200

    taskpoint

---
+++ classpoint/Models/Point.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classpoint.Models
+{
+    internal class Point
+    {
+        int x;
+        int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        static public Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.x + p2.x, p1.y + p2.y);
+
+        }
+        static public Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.x - p2.x, p1.y - p2.y);
+        }
+        public static Point operator *(Point p1, Point p2)
+        {
+            return new Point(p1.x * p2.x, p1.y * p2.y);
+        }
+        public static Point operator /(Point p1, Point p2)
+        {
+            if (p2.x == 0 || p2.y == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return new Point(p1.x / p2.x, p1.y / p2.y);
+        }
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return p1.x == p2.x && p1.y == p2.y;
+        }
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator >(Point p1, Point p2)
+        {
+            return (p1.x > p2.x) && (p1.y > p2.y);
+        }
+        public static bool operator <(Point p1, Point p2)
+        {
+            return (p1.x < p2.x) && (p1.y < p2.y);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Point p)
+                return this == p;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ y.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+    }
+}
---
+++ classpoint/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using classpoint.Models;
+
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+
+
+       Point p1 = new Point(2, 3);
+         Point p2 = new Point(4, 5);
+    Point sum = p1 + p2;
+     Point difference=p1 - p2;
+        Point product=p1 * p2;
+        Point  quotient=p1 / p2;
+        
+        
+
+        Console.WriteLine($"p1: {p1}");
+          Console.WriteLine($"p2: {p2}");
+          Console.WriteLine($"Sum: {sum}");
+          Console.WriteLine($"Difference: {difference}");
+          Console.WriteLine($"Product: {product}");
+          Console.WriteLine($"Quotient: {quotient}");
+        Console.WriteLine($"p1.Equals(p2)? {p1.Equals(p2)}");   
+
+        Console.WriteLine($"p1 HashCode: {p1.GetHashCode()}");
+        Console.WriteLine($"p2 HashCode: {p2.GetHashCode()}");
+    }
+}
+
